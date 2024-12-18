7965136bb61bea95275b57599fc8180e3bbd474f
Kiana Heidari <kiana_hedari71@yahoo.com>
2024-12-17 23:44:21 -0500

    style changed

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,9 +1,8 @@
  const Video = () => {
     return (
-        <div className="w-full h-full flex justify-center flex-wrap items-center overflow-y-auto gap-4 overflow-y-scrollauto">
-            <video className="rounded-xl shadow-lg w-full" src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4" controls></video>
-          
-           
+        <div className="w-full  h-full flex justify-center flex-wrap items-center overflow-y-auto gap-4 overflow-y-scrollauto">
+            <video className="max-h-[500px] rounded-xl shadow-lg w-full" src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4" controls></video>
+        
         </div>
     );
  };
